7c8a630b1f3f8b1252d2e24870e676f7fcd108c3
Michal <michal.nowak3010@gmail.com>
2023-08-01 16:16:56 +0200

    Parent/Child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngTrip';
+  parmes:string = "Zło kutas i zniszczenie";
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { OutletContext } from '@angular/router';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+
+  title:string = "List of Posts";
+  messpost: string = "Message Post";
+
+  @Input() fromParent!:string;
+  
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 
 }
